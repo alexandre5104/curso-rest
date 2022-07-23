c2943ed6377c632549cb4a5e0152f0a517ca2ff6
francisco alexandre batista da silva <alexandrebatistaril@gmail.com>
2022-07-22 21:35:43 -0300

    Projeto criado

---
+++ src/main/java/com/alexandre/CursoRestApplication.java
@@ -0,0 +1,13 @@
+package com.alexandre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoRestApplication.class, args);
+	}
+
+}
